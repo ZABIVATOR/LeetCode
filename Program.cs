c3f054405b2e1c9f800b83f8b9c3d8e0349e11a4
priym <priym@DESKTOP-UQL3HVH>
2024-01-04 18:23:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode
+{
+    class Test : Solution
+    {
+        static void Main()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, };
+            Console.WriteLine(MinOperations(nums));
+        }
+    }
+}
