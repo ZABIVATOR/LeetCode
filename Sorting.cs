eb3a34d48c2ec8baa836d9488eb345f85be081ee
Александр Часовской <priymenko_a@mail.ru>
2024-01-05 22:29:42 +0300

    164

--- Sorting.cs
+++ Sorting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LeetCode
 {
@@ -142,26 +143,177 @@ namespace LeetCode
         static public int[] Task2343SmallestTrimmedNumbers(string[] nums, int[][] queries)//2343. Query Kth Smallest Trimmed Number
         {
             List<int> res = new List<int>();
-            foreach (int[] q in queries) {
+            foreach (int[] q in queries)
+            {
                 int min = Task2343Countonequerry(nums, q[0], q[1]);
-               // res.Add(int.Parse(nums[min]));
-               res.Add(min);
+                // res.Add(int.Parse(nums[min]));
+                res.Add(min);
             }
             return res.ToArray();
         }
-        
-        static int Task2343Countonequerry(string[] nums,int k, int trim)
+
+        static int Task2343Countonequerry(string[] nums, int k, int trim)
         {
-            int[] temptrimmed = new int[nums.Length];
-            Dictionary<int,int> result = new Dictionary<int,int>();
+            int eachNumlenght = nums[0].Length;
+            List<(string, int)> values = new List<(string, int)>();
             for (int i = 0; i < nums.Length; i++)
             {
-                result.Add(i, int.Parse(nums[i].Substring(nums[i].Length - trim)));
+                values.Add((nums[i].Substring(eachNumlenght - trim), i));
             }
-            result = result.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-            return result.ElementAt(k-1).Key;
+            values.Sort((a, b) => a.Item1 != b.Item1 ? a.Item1.CompareTo(b.Item1) : a.Item2.CompareTo(b.Item2));
+            return values.Skip(k - 1).ToList()[0].Item2;
         }
 
+        public void countingSort(int[] arr)
+        {
+            // Sorts an array of integers where minimum value is 0 and maximum value is K
+            int K = arr.Max();
+            int[] counts = new int[K + 1];
+            foreach (int elem in arr)
+            {
+                counts[elem] += 1;
+            }
+            // we now overwrite our original counts with the starting index
+            // of each element in the final sorted array
+            int startingIndex = 0;
+            for (int i = 0; i < K + 1; i++)
+            {
+                int count = counts[i];
+                counts[i] = startingIndex;
+                startingIndex += count;
+            }
 
+            int[] sortedArray = new int[arr.Length];
+            foreach (int elem in arr)
+            {
+                sortedArray[counts[elem]] = elem;
+                // since we have placed an item in index counts[elem], we need to
+                // increment counts[elem] index by 1 so the next duplicate element
+                // is placed in appropriate index
+                counts[elem] += 1;
+            }
+
+            // common practice to copy over sorted list into original arr
+            // it's fine to just return the sortedArray at this point as well
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = sortedArray[i];
+            }
+        }
+
+        private static int NUM_DIGITS = 10;
+
+        public void countingSort(int[] arr, int placeVal)
+        {
+            // Sorts an array of integers where minimum value is 0 and maximum value is NUM_DIGITS
+            int[] counts = new int[NUM_DIGITS];
+
+            foreach (int elem in arr)
+            {
+                int current = elem / placeVal;
+                counts[current % NUM_DIGITS] += 1;
+            }
+
+            // we now overwrite our original counts with the starting index
+            // of each digit in our group of digits
+            int startingIndex = 0;
+            for (int i = 0; i < counts.Length; i++)
+            {
+                int count = counts[i];
+                counts[i] = startingIndex;
+                startingIndex += count;
+            }
+
+            int[] sortedArray = new int[arr.Length];
+            foreach (int elem in arr)
+            {
+                int current = elem / placeVal;
+                sortedArray[counts[current % NUM_DIGITS]] = elem;
+                // since we have placed an item in index counts[current % NUM_DIGITS],
+                // we need to increment counts[current % NUM_DIGITS] index by 1 so the
+                // next duplicate digit is placed in appropriate index
+                counts[current % NUM_DIGITS] += 1;
+            }
+
+            // common practice to copy over sorted list into original arr
+            // it's fine to just return the sortedArray at this point as well
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = sortedArray[i];
+            }
+        }
+
+        public void radixSort(ref int[] arr)
+        {
+            int maxElem = int.MinValue;
+            foreach (int elem in arr)
+            {
+                if (elem > maxElem)
+                {
+                    maxElem = elem;
+                }
+            }
+
+            int placeVal = 1;
+            while (maxElem / placeVal > 0)
+            {
+                countingSort(arr, placeVal);
+                placeVal *= 10;
+            }
+        }
+
+        public int Task164_MaximumGap_outofmemory(int[] nums)//counting not working
+        {
+            int max = 0;
+            if (nums.Length < 2)
+            {
+                return max;
+            }
+            int K = nums.Max();
+            int[] counts = new int[K + 1];
+            int lastnotzeroelement = 0;
+            foreach (int elem in nums)
+            {
+                counts[elem] += 1;
+
+            }
+
+            for (int i = 0; i < K + 1; i++)
+            {
+                if (counts[i] != 0)
+                {
+                    lastnotzeroelement = i;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < K + 1; i++)
+            {
+                if (counts[i] != 0)
+                {
+                    if (Math.Abs(i - lastnotzeroelement) > max)
+                    {
+                        max = i - lastnotzeroelement;
+                    }
+                    lastnotzeroelement = i;
+                }
+            }
+
+            return max;
+        }
+
+        public int Task164_MaximumGap(int[] nums)
+        {
+            radixSort(ref nums);
+            int max = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (Math.Abs(nums[i] - nums[i - 1]) > max)
+                {
+                    max = nums[i] - nums[i - 1];
+                }
+            }
+            return max;
+        }
     }
 }
