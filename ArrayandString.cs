df3264d93f1ee83e7af85300633b2c58e44fa5b9
ZABIVATOR <flameandfire2002@gmail.com>
2024-01-06 22:49:09 +0300

    209

--- ArrayandString.cs
+++ ArrayandString.cs
@@ -10,7 +10,31 @@ namespace LeetCode
 {
     internal interface IArrayandString
     {
-        
+
+        static public int Task209_MinSubArrayLen(int target, int[] nums)
+        {
+            int start = 0;
+            int end = 0;
+            int w_sum = nums[0];
+            int maxLen = int.MaxValue;
+            while (end < nums.Length)
+            {
+                if (w_sum < target)
+                {
+                    end++;
+                    if (end < nums.Length)
+                        w_sum += nums[end];
+                }
+                else
+                {
+                    maxLen = Math.Min(maxLen, end - start + 1);
+                    w_sum -= nums[start];
+                    start++;
+                }
+            }
+            return maxLen == int.MaxValue ? 0 : maxLen;
+        }
+
         public int Task485_FindMaxConsecutiveOnes(int[] nums)
         {
             int count = 0;
--- Program.cs
+++ Program.cs
@@ -8,10 +8,8 @@
 
             int[][] matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
 
-            //foreach (var x in temp) { Console.Write(x); }
-            string[] s = ["h", "e", "l", "l", "o"];
-            Array.Reverse(s);
-            foreach (var x in s) { Console.Write(x); }
+            var temp = IArrayandString.Task209_MinSubArrayLen(213, [12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12]);
+            Console.WriteLine(temp);
 
         }
     }
