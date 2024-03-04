c4bcdbd7c22add8ea0a055b7b5c73a9f5f05b1a6
ZABIVATOR <flameandfire2002@gmail.com>
2024-03-04 21:09:29 +0300

    948

--- DailySolutions.cs
+++ DailySolutions.cs
@@ -9,6 +9,34 @@ namespace LeetCode
 {
     internal interface IDailySolutions: ISorting,IArrayandString
     {
+        public int Task948_BagOfTokensScore(int[] tokens, int power)
+        {
+            Array.Sort(tokens);
+            int maxScore = 0, currentScore = 0;
+            int left = 0, right = tokens.Length - 1;
+
+            while (left <= right)
+            {
+                if (power >= tokens[left])
+                {
+                    power -= tokens[left++];
+                    currentScore++;
+                    maxScore = Math.Max(maxScore, currentScore);
+                }
+                else if (currentScore > 0)
+                {
+                    power += tokens[right--];
+                    currentScore--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return maxScore;
+        }
+
         public ListNode Task19_RemoveNthFromEnd(ListNode head, int n)
         {
             int length = findLength(head);
